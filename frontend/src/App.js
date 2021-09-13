b09139a1bb067a025cfb3b36fa1758205d09f1c3
Halil Özat <halilozat@hotmail.com>
2021-09-13 18:00:44 +0300

    added dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './components/Header';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return;
-  <>
-    <Header />
-  </>;
+  const [macros, setMacros] = useState({
+    carb: 200,
+    protein: 100,
+    fat: 50,
+  });
+
+  return (
+    <div>
+      <Header macros={macros} />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Header = ({ macros }) => {
+  return (
+    <>
+      <div>
+        Günlük Makrolarınız:
+        <br />
+        <br />
+        Karbonhidrat: {macros.carb} gr. daha alabilirsiniz.
+        <br />
+        Protein: {macros.protein} gr. daha alabilirsiniz.
+        <br />
+        Yağ: {macros.fat} gr. daha alabilirsiniz.
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
