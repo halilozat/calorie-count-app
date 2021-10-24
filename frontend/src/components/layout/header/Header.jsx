d20230fd5b3ea2bd37fb0a18fda8af52b1fcf438
Halil Özat <halilozat@hotmail.com>
2021-10-24 14:01:32 +0300

    added responsive for home screen and header component

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -7,11 +7,10 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '../../../context/AuthContext/AuthContext';
 
 /** Images */
-import logoImg from '../../../assets/images/logo.png'
+import logoImg from '../../../assets/images/dot.png'
 
 /** Styles */
 import './header.scss'
-
 
 
 const Header = () => {
@@ -33,14 +32,16 @@ const Header = () => {
                         <div className="col">
                             <div className="header_content d-flex flex-row align-items-center justify-content-start trans_400">
                                 <a href="#">
-                                    <div className="logo d-flex flex-row align-items-center justify-content-start"><img className="logoImg" src={logoImg} alt="" /><div>Fit<span>Foods</span></div></div>
+                                    <div className="logo d-flex flex-row align-items-center justify-content-start">
+                                        <img src={logoImg} alt="" />
+                                        <div>Fit<span>Foods</span></div>
+                                    </div>
                                 </a>
                                 <nav className="main_nav">
                                     <ul className="d-flex flex-row align-items-center justify-content-start">
-                                        <li><a href="index.html">Home</a></li>
+                                        <li ><a href="index.html">Home</a></li>
                                         <li><a href="services.html">Meal Calendar</a></li>
                                         <li><a href="blog.html">My Macros</a></li>
-                                        <li><a href="blog.html" className="phone d-flex flex-row align-items-center justify-content-start ml-auto"></a></li>
                                     </ul>
                                 </nav>
                                 <div className="phone d-flex flex-row align-items-center justify-content-start ml-auto">
@@ -52,6 +53,33 @@ const Header = () => {
                 </div>
             </header>
 
+            <div className="hamburger_bar trans_400 d-flex flex-row align-items-center justify-content-start">
+                <div className="hamburger">
+                    <div className="menu_toggle d-flex flex-row align-items-center justify-content-start">
+                        <span>menu</span>
+                        <div className="hamburger_container">
+                            <div className="menu_hamburger">
+                                <div className="line_1 hamburger_lines" style={{ transform: "matrix(1, 0, 0, 1, 0, 0)" }}></div>
+                                <div className="line_2 hamburger_lines" style={{ visibility: "inherit", opacity: 1 }}></div>
+                                <div className="line_3 hamburger_lines" style={{ transform: "matrix(1, 0, 0, 1, 0, 0)" }}></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="menu trans_800">
+                <div className="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+                    <ul className="d-flex flex-row align-items-center justify-content-start">
+                        <li className="active"><a href="index.html">Home</a></li>
+                        <li><a href="services.html">Meal Calendar</a></li>
+                        <li><a href="blog.html">My Macros</a></li>
+                    </ul>
+                </div>
+                <div className="menu_phone d-flex flex-row align-items-center justify-content-start">
+                    <span>LOGIN</span>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -7,9 +7,12 @@ import Header from '../../components/Layout/Header/Header';
 import Icon1 from '../../assets/images/icon_1.png'
 import Icon2 from '../../assets/images/icon_2.png'
 import Icon3 from '../../assets/images/icon_3.png'
+import Icon4 from '../../assets/images/icon_4.png'
+import Icon8 from '../../assets/images/icon_8.png'
 
 /** Styles */
 import './home.scss'
+import './responsive.scss'
 
 
 
@@ -34,7 +37,7 @@ const Home = () => {
                                         </a>
                                     </div>
                                     <div className="home_title">Get fit with us</div>
-                                    <button className="joinButton home_button ml-auto mr-auto"><a href="#">Join Now</a></button>
+                                    <button className="button home_button ml-auto mr-auto"><a href="#">Join Now</a></button>
                                 </div>
                             </div>
                         </div>
@@ -49,11 +52,43 @@ const Home = () => {
                             <div className="boxes_container d-flex flex-lg-row flex-column align-items-start justify-content-start">
 
                                 <div className="box">
+                                    <div className="box_icon d-flex flex-column align-items-center justify-content-center"><img src={Icon4} alt="" /></div>
+                                    <div className="box_title">8 weeks nutrition program</div>
+                                    <div className="box_text">
+                                        <p>Coming Soon...</p>
+                                        <br />
+                                        <br />
+                                    </div>
+                                    <div className="box_link_container">
+                                        <a href="#"><div className="box_link d-flex flex-column align-items-center justify-content-center trans_200">
+                                            <div>+</div>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="box">
+                                    <div className="box_icon d-flex flex-column align-items-center justify-content-center"><img src={Icon8} alt="" /></div>
+                                    <div className="box_title">Track your meals from the calendar</div>
+                                    <div className="box_text">
+                                        <p>Go to your meals calendar.</p>
+                                        <br />
+                                        <br />
+                                    </div>
+                                    <div className="box_link_container">
+                                        <a href="#"><div className="box_link d-flex flex-column align-items-center justify-content-center trans_200">
+                                            <div>+</div>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="box">
                                     <div className="box_icon d-flex flex-column align-items-center justify-content-center"><img src={Icon1} alt="" /></div>
-                                    <div className="box_title">Pilates with trainer</div>
+                                    <div className="box_title">8 weeks training program</div>
                                     <div className="box_text">
-                                        <p>Etiam commodo justo nec aliquam feugiat. Donec a leo eget augue porttitor sollicitudin.</p>
+                                        <p>Coming Soon...</p>
+                                        <br /><br />
                                     </div>
+
                                     <div className="box_link_container">
                                         <a href="#"><div className="box_link d-flex flex-column align-items-center justify-content-center trans_200">
                                             <div>+</div>
@@ -62,33 +97,7 @@ const Home = () => {
                                     </div>
                                 </div>
 
-                                <div className="box">
-                                    <div className="box_icon d-flex flex-column align-items-center justify-content-center"><img src={Icon2} alt="" /></div>
-                                    <div className="box_title">Swimming Pool</div>
-                                    <div className="box_text">
-                                        <p>Donec a leo eget augue porttitor sollicitudin. Morbi sed varius risus, vitae molestie lectus. Donec id hendrerit.</p>
-                                    </div>
-                                    <div className="box_link_container">
-                                        <a href="#"><div className="box_link d-flex flex-column align-items-center justify-content-center trans_200">
-                                            <div>+</div>
-                                        </div>
-                                        </a>
-                                    </div>
-                                </div>
 
-                                <div className="box">
-                                    <div className="box_icon d-flex flex-column align-items-center justify-content-center"><img src={Icon3} alt="" /></div>
-                                    <div className="box_title">Healthy diet plan</div>
-                                    <div className="box_text">
-                                        <p>Morbi sed varius risus, vitae molestie lectus. Donec id hendrerit velit, eu fringilla neque.</p>
-                                    </div>
-                                    <div className="box_link_container">
-                                        <a href="#"><div className="box_link d-flex flex-column align-items-center justify-content-center trans_200">
-                                            <div>+</div>
-                                        </div>
-                                        </a>
-                                    </div>
-                                </div>
 
                             </div>
                         </div>
