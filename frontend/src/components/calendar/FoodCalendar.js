be412b6f3bb68c0ea75550a5eac009f189b2c2d9
Halil Özat <halilozat@hotmail.com>
2021-10-31 20:25:27 +0300

    added food datas for calendar

--- frontend/src/components/calendar/FoodCalendar.js
+++ frontend/src/components/calendar/FoodCalendar.js
@@ -23,91 +23,19 @@ import './foodCalendar.scss'
 let id = 0;
 
 const FoodCalendar = () => {
-    const [initialEvents, setInitialEvents] = useState([
-        {
-            id: 10001,
-            title: "Hello World",
-            start: new Date().toISOString().split("T")[0]
-        },
-        {
-            id: 20002,
-            title: "Coffee Time",
-            start: new Date().toISOString().split("T")[0] + "T14:05:00"
-        },
-        {
-            id: 20003,
-            title: "Lunch",
-            start: new Date().toISOString().split("T")[0] + "T14:35:00",
-            end: new Date().toISOString().split("T")[0] + "T17:35:00"
-        },
-        { title: 'Hunger Games', date: '2021-10-05', color: "red" },
-        { title: 'Hunger Games', date: '2021-10-06', color: "purple" },
-        { title: 'Intermittent fasting', date: '2021-10-06', color: "brown" },
-        {
-            title: 'Breakfast',
-            start: '2021-10-01T08:30:00',
-            end: '2021-10-01T09:30:00',
-            allDay: false
-        },
-        {
-            title: 'Coffee Time',
-            start: '2021-10-01T09:00:00',
-            end: '2021-10-01T09:30:00',
-            allDay: false
-        },
-        {
-            title: 'Lunch',
-            date: '2021-10-01',
-            start: '2021-10-01 12:30:00',
-            end: '2021-10-01 13:00:00',
-            allDay: false
-        },
-        {
-            title: 'Dinner',
-            date: '2021-10-01',
-            start: '2021-10-01T18:30:00',
-            end: '2021-10-01T19:00:00',
-            allDay: false
-        },
-        {
-            title: 'Breakfast',
-            start: '2021-10-02T08:30:00',
-            end: '2021-10-02T09:30:00',
-        },
-        {
-            title: 'Lunch',
-            start: '2021-10-02 12:30:00',
-            end: '2021-10-02 13:00:00',
-        },
-        {
-            title: 'Dinner',
-            start: '2021-10-02T18:30:00',
-            end: '2021-10-02T19:00:00',
-        }
-    ])
-
+    const [events, setEvents] = useState([])
     const calendarRef = useRef(null)
     const { userId } = useAuth()
-
-    useEffect(() => {
-
-        const getUserFoods = async () => {
-            await CalorieCountService.GetUserFoods(userId)
-                .then(res => setInitialEvents(...initialEvents, [{
-                    id: res.data[0].userId,
-                    title: res.data[0].foodname,
-                    start: res.data[0].createDateTime.toISOString()
-                }]))
-                .catch(err => console.log(err))
-        }
-
-        getUserFoods()
-    }, [])
 
     const handleEventClick = (clickInfo) => {
         alertify.alert("Event Info!", `This Event's Title : ${clickInfo.event.title}`)
         console.log(clickInfo.event.id)
         // clickInfo.event.remove()
+    }
+
+    async function handleDatesSet(data) {
+        const response = await CalorieCountService.GetUserFoods(userId)
+        setEvents(response.data)
     }
 
     const handleDateSelect = (selectInfo) => {
@@ -130,15 +58,16 @@ const FoodCalendar = () => {
             <div className="foodCalendar">
                 <AddFoodModal />
                 <FullCalendar
+                    events={events}
                     ref={calendarRef}
                     plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
                     initialView="dayGridMonth"
-                    initialEvents={initialEvents}
                     eventClick={handleEventClick}
                     select={handleDateSelect}
                     dateClick={(e) => {
                         console.log("dateclick", e)
                     }}
+                    datesSet={(date) => handleDatesSet(date)}
                     firstDay={1}
                     selectable={true}
                     editable={true}
